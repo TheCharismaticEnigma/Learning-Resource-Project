5def7d33ce2456ffb8631661c090bb5d09313351
TheCharismaticEnigma <avishkar2002@mail.com>
2023-02-23 19:42:55 +0530

    CheckPoint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <!-- Main App component/Root Component. -->
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          name: 'Official Guide',
+          description: 'The Official Vue.js Documentation.',
+          link: 'https://vuejs.org',
+        },
+
+        {
+          id: 'google',
+          name: 'Google',
+          description: 'Google that bitch! AHAHAHAHA! .',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <div></div>
+  <ul v-if="storedResources.length > 0">
+    <li v-for="resource in storedResources" :key="id">
+      <h2>{{ resource.name }}</h2>
+      <p>{{ resource.description }}</p>
+      <a :href="resource.link"> {{ resource.link }} </a>
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
+<!-- This contains GLOBAL APP STYLES -->
+
+<style></style>
